7dea9b1c396b7f727b18d1a93ac23ea06a69f7dd
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-05-31 21:28:13 +0000

    brew(remote-monitor): update to v0.1.4

--- Formula/remote-monitor.rb
+++ Formula/remote-monitor.rb
@@ -1,30 +1,30 @@
 class RemoteMonitor < Formula
   desc "Terminal UI for monitoring a remote Linux host over SSH"
   homepage "https://github.com/LMLiam/remote-monitor"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.3/remote-monitor_0.1.3_darwin_arm64.tar.gz"
-      sha256 "06433559f59deda2dc1d535109e06392ab47575546e5f4ccb16c9c6d734f24e8"
+      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.4/remote-monitor_0.1.4_darwin_arm64.tar.gz"
+      sha256 "fbcf4cb9064496f2e7bfe3c85c4879f1b60ce31418388222856599d29496c408"
     end
 
     on_intel do
-      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.3/remote-monitor_0.1.3_darwin_amd64.tar.gz"
-      sha256 "da468467356fa1252ab17084e9ba622fed17914d1aadf591ddcddeddbcd744cf"
+      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.4/remote-monitor_0.1.4_darwin_amd64.tar.gz"
+      sha256 "5aa46b61c0fc770ad569bc0223af5116c064b7c6d5bc69f7fb9408b3f1437042"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.3/remote-monitor_0.1.3_linux_arm64.tar.gz"
-      sha256 "e5f165752c46881d9a53f475714aa11f4b87bd3d238368efd3b7c2ce4c521214"
+      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.4/remote-monitor_0.1.4_linux_arm64.tar.gz"
+      sha256 "cacd4cb979a940c2b186774b6b64e9120f89f5c89b5a50eaf1e2fd96da675d6e"
     end
 
     on_intel do
-      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.3/remote-monitor_0.1.3_linux_amd64.tar.gz"
-      sha256 "15153a48f7a2197e7ff04f36ef68d59219d6fcdf6dad8d3b9c84e026fdb1c43b"
+      url "https://github.com/LMLiam/remote-monitor/releases/download/v0.1.4/remote-monitor_0.1.4_linux_amd64.tar.gz"
+      sha256 "77297dc8f24a55e9827bd8d422c1f0d9f4732fe6daba91361ea0fd91aa77a693"
     end
   end
 
